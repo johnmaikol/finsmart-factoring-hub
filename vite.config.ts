c3c183f800655f44198cfee76a625c9793213ece
John A <80300828+johnmaikol@users.noreply.github.com>
2025-03-01 23:52:33 -0500

    gf

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  plugins: [react()],
   base: "/finsmart-factoring-hub/",  // 🔹 Agregado para GitHub Pages
   server: {
     host: "::",
